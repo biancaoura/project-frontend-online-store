12791a2a875feb27fdbab9b8192a78ac2e7f6f5c
Bianca Oura <biancaoura@gmail.com>
2022-08-05 16:41:27 -0300

    Requisito 4

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
 export default class Home extends Component {
     state = {
       item: [],
+      categories: [],
+    }
+
+    async componentDidMount() {
+      const response = await getCategories();
+      this.setState({
+        categories: response,
+      });
     }
 
     render() {
-      const { item } = this.state;
+      const { item, categories } = this.state;
       return (
         <section>
           <div>
             <Link to="/cart" data-testid="shopping-cart-button">Cart</Link>
           </div>
+          <section>
+            <select name="category">
+              { categories.map(({ id, name }) => (
+                <option name="category" key={ id } data-testid="category">
+                  {name}
+                </option>
+              ))}
+            </select>
+          </section>
           <form>
             <label htmlFor="home">
               <input type="text" id="home" />
