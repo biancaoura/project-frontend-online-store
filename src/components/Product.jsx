4c25ac4b268d1534b32019557984c42661c6642c
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2022-08-05 21:26:15 -0300

    localstorage

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,17 @@
 import { shape } from 'prop-types';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import setToLocal from '../services/localStorage';
 
 export default class Product extends Component {
+  handleClick = () => {
+    const { product } = this.props;
+    const productObj = {
+      ...product,
+    };
+    setToLocal(productObj);
+  }
+
   render() {
     const { product } = this.props;
     const { title, thumbnail, price, id } = product;
@@ -18,6 +27,14 @@ export default class Product extends Component {
           Mais detalhes
 
         </Link>
+        <button
+          data-testid="product-add-to-cart"
+          type="button"
+          onClick={ this.handleClick }
+        >
+          Adicionar ao carrinho
+
+        </button>
       </div>
     );
   }
---
+++ src/services/localStorage.js
@@ -0,0 +1,59 @@
+const key = 'cartItems';
+
+if (!JSON.parse(localStorage.getItem(key))) {
+  localStorage.setItem(key, JSON.stringify([]));
+}
+
+const readLocal = () => localStorage.getItem(key);
+const setLocal = (cart) => localStorage
+  .setItem(key, JSON.stringify(cart));
+
+export default function setToLocal(obj) {
+  if (obj) {
+    const oldLocal = readLocal();
+    setLocal([...oldLocal, obj]);
+  }
+}
+
+// const FAVORITE_SONGS_KEY = 'favorite_songs';
+// const TIMEOUT = 500;
+// const SUCCESS_STATUS = 'OK';
+
+// if (!JSON.parse(localStorage.getItem(FAVORITE_SONGS_KEY))) {
+//   localStorage.setItem(FAVORITE_SONGS_KEY, JSON.stringify([]));
+// }
+// const readFavoriteSongs = () => JSON.parse(localStorage.getItem(FAVORITE_SONGS_KEY));
+
+// const saveFavoriteSongs = (favoriteSongs) => localStorage
+//   .setItem(FAVORITE_SONGS_KEY, JSON.stringify(favoriteSongs));
+
+// // --------------------------------------------------------------------
+// // A função simulateRequest simula uma requisição para uma API externa
+// // Esse tipo de função que "chama outra função" é chamada de
+// // "currying function" https://javascript.info/currying-partials
+// // não se preocupe, estudaremos isso futuramente.
+// // --------------------------------------------------------------------
+
+// const simulateRequest = (response) => (callback) => {
+//   setTimeout(() => {
+//     callback(response);
+//   }, TIMEOUT);
+// };
+
+// export const getFavoriteSongs = () => new Promise((resolve) => {
+//   const favoriteSongs = readFavoriteSongs();
+//   simulateRequest(favoriteSongs)(resolve);
+// });
+
+// export const addSong = (song) => new Promise((resolve) => {
+//   if (song) {
+//     const favoriteSongs = readFavoriteSongs();
+//     saveFavoriteSongs([...favoriteSongs, song]);
+//   }
+//   simulateRequest(SUCCESS_STATUS)(resolve);
+// });
+
+// export const removeSong = (song) => {
+//   const favoriteSongs = readFavoriteSongs();
+//   saveFavoriteSongs(favoriteSongs.filter((s) => s.trackId !== song.trackId));
+// };
