60589664b76843fcbb0db72cf58414cf4e08305a
Bianca Oura <biancaoura@gmail.com>
2022-08-09 11:21:11 -0300

    Requisito 12

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Home from './components/Home';
 import ProductCard from './components/ProductCard';
 import ShoppingCart from './components/ShoppingCart';
+import Checkout from './components/Checkout';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
           <Route exact path="/" component={ Home } />
           <Route exact path="/cart/:id" component={ ShoppingCart } />
           <Route exact path="/product/:id" component={ ProductCard } />
+          <Route exact path="/checkout" component={ Checkout } />
         </Switch>
       </BrowserRouter>
     );
---
+++ src/components/Checkout.jsx
@@ -0,0 +1,160 @@
+import { shape } from 'prop-types';
+import React, { Component } from 'react';
+import { getSavedCart } from '../services/localStorage';
+
+export default class Checkout extends Component {
+  state = {
+    cartItems: [],
+    filteredCart: [],
+    name: '',
+    email: '',
+    cpf: '',
+    phone: '',
+    cep: '',
+    address: '',
+    checked: false,
+    isValid: true,
+  }
+
+  componentDidMount() {
+    const cartItems = getSavedCart();
+    // console.log(cartItems);
+    const filteredCart = cartItems
+      .filter((item, index, array) => index === array
+        .findIndex((objt) => objt.id === item.id));
+    this.setState({ cartItems, filteredCart });
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    if (target.type === 'radio') this.setState({ checked: true });
+    this.setState({ [name]: value });
+  }
+
+  handleClick = (e) => {
+    e.preventDefault();
+    const { name, email, cpf, phone, cep, address, checked } = this.state;
+    const { history } = this.props;
+    if (!name || !email || !cpf || !phone || !cep || !address || !checked) {
+      return this.setState({ isValid: false });
+    }
+    localStorage.removeItem('cart_items');
+    return history.push('/');
+  }
+
+  render() {
+    const {
+      cartItems, filteredCart, name, email, cpf, phone, cep, address, checked, isValid,
+    } = this.state;
+    const { handleChange, handleClick } = this;
+
+    return (
+      <div>
+        { filteredCart.map((item, index) => (
+          <section key={ index }>
+            <p>{item.title}</p>
+            <p>{item.price}</p>
+            {/* <p>{cartItems.map((itm) => itm.id === item.id).length}</p> */}
+          </section>
+        )) }
+        <span>
+          {`Total: R$${cartItems.reduce((acc, { price }) => acc + price, 0).toFixed(2)}`}
+        </span>
+        <form>
+          <input
+            type="text"
+            placeholder="Nome completo"
+            name="name"
+            value={ name }
+            onChange={ handleChange }
+            data-testid="checkout-fullname"
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={ email }
+            onChange={ handleChange }
+            data-testid="checkout-email"
+          />
+          <input
+            type="text"
+            placeholder="CPF"
+            name="cpf"
+            value={ cpf }
+            onChange={ handleChange }
+            data-testid="checkout-cpf"
+          />
+          <input
+            type="text"
+            placeholder="Telefone"
+            name="phone"
+            value={ phone }
+            onChange={ handleChange }
+            data-testid="checkout-phone"
+          />
+          <input
+            type="text"
+            placeholder="CEP"
+            name="cep"
+            value={ cep }
+            onChange={ handleChange }
+            data-testid="checkout-cep"
+          />
+          <input
+            type="text"
+            placeholder="Endereço"
+            name="address"
+            value={ address }
+            onChange={ handleChange }
+            data-testid="checkout-address"
+          />
+          <div>
+            <input
+              type="radio"
+              name="payment"
+              checked={ checked }
+              onChange={ handleChange }
+              data-testid="ticket-payment"
+            />
+            <input
+              type="radio"
+              name="payment"
+              checked={ checked }
+              onChange={ handleChange }
+              data-testid="visa-payment"
+            />
+            <input
+              type="radio"
+              name="payment"
+              checked={ checked }
+              onChange={ handleChange }
+              data-testid="master-payment"
+            />
+            <input
+              type="radio"
+              name="payment"
+              checked={ checked }
+              onChange={ handleChange }
+              data-testid="elo-payment"
+            />
+          </div>
+          {
+            !isValid && <p data-testid="error-msg">Campos inválidos</p>
+          }
+          <button
+            type="submit"
+            onClick={ handleClick }
+            data-testid="checkout-btn"
+          >
+            Comprar
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+Checkout.propTypes = {
+  history: shape().isRequired,
+};
