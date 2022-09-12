af96cecbf4f0112ef538166261db8e731f292373
Bianca Oura <biancaoura@gmail.com>
2022-09-12 01:08:41 -0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Route, Switch, HashRouter as Router } from 'react-router-dom';
 import Home from './components/Home';
 import ProductCard from './components/ProductCard';
 import ShoppingCart from './components/ShoppingCart';
@@ -8,14 +8,14 @@ import Checkout from './components/Checkout';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <Router>
         <Switch>
           <Route exact path="/" component={ Home } />
           <Route exact path="/cart/:id" component={ ShoppingCart } />
           <Route exact path="/product/:id" component={ ProductCard } />
           <Route exact path="/checkout" component={ Checkout } />
         </Switch>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
